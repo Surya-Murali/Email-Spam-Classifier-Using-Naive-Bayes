8b67962a22565b0d9a5c13970642547928dc2e4b
Surya Murali <msurya25@gmail.com>
2018-01-09 23:50:52 -0500

    First commit

---
+++ GaussianNB.py
@@ -0,0 +1,19 @@
+import numpy as np
+from sklearn.naive_bayes import GaussianNB
+
+#Assigning features and target variables
+#Training data
+features = np.array([[1,2],[3,4], [5,6], [-1,-2], [-3,-4], [-5,-6]])
+target = np.array([10, 20, 20, 10, 20, 20])
+
+#Creating a Gaussian Classifier
+model = GaussianNB()
+
+#Train the model using training data
+model.fit(features, target)
+
+#Predict Output 
+predictedOutput = model.predict([[3,2],[1,3]])
+print (predictedOutput)
+
+#Output: ([3,4])
